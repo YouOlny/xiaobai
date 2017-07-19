996e2bd9dd3ac280427acf2f8e7c795f72a46345
xiaobai <studycow@163.com>
2017-07-19 16:09:50 +0800

    哈哈

---
+++ test/test/test2ViewController.m
@@ -0,0 +1,37 @@
+//
+//  test2ViewController.m
+//  test
+//
+//  Created by cow on 2017/7/19.
+//  Copyright © 2017年 cow. All rights reserved.
+//
+
+#import "test2ViewController.h"
+
+@interface test2ViewController ()
+
+@end
+
+@implementation test2ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
